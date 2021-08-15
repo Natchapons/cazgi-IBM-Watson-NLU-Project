79cbdf6a09fc1b48b5efa9ba4796cc4d6feb812c
natchapons <natchapon.stk@gmail.com>
2021-08-15 13:40:06 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = new express();
 const dotenv = require('dotenv');
+const title = 'Sentiment Analyzer'
 dotenv.config();
 
 function getNLUInstance(){
@@ -17,6 +18,31 @@ function getNLUInstance(){
         }),
         serviceUrl: api_url,
     });
+
+    const analyzeParams = {
+      'url': 'www.ibm.com',
+      'features': {
+        'entities': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 2,
+        },
+        'keywords': {
+          'emotion': true,
+          'sentiment': true,
+          'limit': 2,
+        },
+      },
+    };
+    
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+      .then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2));
+      })
+      .catch(err => {
+        console.log('error:', err);
+      });
+
     return naturalLanguageUnderstanding;
 }
 
@@ -31,7 +57,7 @@ app.get("/",(req,res)=>{
 
 app.get("/url/emotion", (req,res) => {
 
-    return res.send({"happy":"90","sad":"10"});
+    return res.send({"sadness":"0.250624","joy":"0.722962","fear":"0.009157","disgust":"0.003132","anger":"0.016098"});
 });
 
 app.get("/url/sentiment", (req,res) => {
@@ -39,7 +65,18 @@ app.get("/url/sentiment", (req,res) => {
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    const nlu= getNLUInstance();
+    var parameters= { text: "I am having a bad day today, \
+        could someone help me please",features : {emotion: {}}}
+    ret= nlu.analyze( parameters ) //, function( error, response){
+           .then (analysisResults=> {
+            console.log(JSON.stringify(analysisResults, null, 2));
+            console.log( "in here")
+           })
+           .catch(err=> {console.log( "error",err);
+           });
+    console.log( ret );
+    return res.send({"sadness":"0.250624","joy":"0.722962","fear":"0.009157","disgust":"0.003132","anger":"0.016098"});
 });
 
 app.get("/text/sentiment", (req,res) => {
